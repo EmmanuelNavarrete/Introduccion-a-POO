6ca628e644473e5644832ceb56abc5e28663ff27
EmmanuelNavarrete <s18030188@cdhidalgo.tecnm.mx>
2023-04-14 16:19:02 -0600

    POO

---
+++ Proyecto-ByteBank/banco/src/CrearCuenta.java
@@ -0,0 +1,9 @@
+public class CrearCuenta {
+    public static void main(String[] args) {
+    
+        cuenta primeraCuenta = new cuenta();
+
+        primeraCuenta.saldo = 200;
+
+    }
+}
---
+++ Proyecto-ByteBank/banco/src/cuenta.java
@@ -0,0 +1,8 @@
+class cuenta {
+
+    double saldo;
+    int agencia;
+    int numero;
+    String titular;
+
+}
